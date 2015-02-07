357dfe557dcf06796669876bf8aeab9997920d1c
aresteban1 <aresteban1@gmail.com>
2015-02-07 15:18:32 +0000

    Manual upload of database.

---
+++ application/bookabus/app/Config/database.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class DATABASE_CONFIG {
+
+public $default = array(
+'datasource' => 'Database/Mysql',
+'persistent' => false,
+'host' => 'localhost',
+'login' => 'root',
+'password' => '',
+'database' => 'bookabus',
+'prefix' => '',
+//'encoding' => 'utf8',
+);
+
+public $test = array(
+'datasource' => 'Database/Mysql',
+'persistent' => false,
+'host' => 'localhost',
+'login' => 'user',
+'password' => 'password',
+'database' => 'test_database_name',
+'prefix' => '',
+//'encoding' => 'utf8',
+);
+}
