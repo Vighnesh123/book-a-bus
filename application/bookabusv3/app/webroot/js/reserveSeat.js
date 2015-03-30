128ff2f555d1eb2a0c3917e96856aba6195eb4c6
aresteban <aresteban1@gmail.com>
2015-03-31 00:21:11 +0800

    Disables taken seats from the layout

--- application/bookabusv3/app/webroot/js/reserveSeat.js
+++ application/bookabusv3/app/webroot/js/reserveSeat.js
@@ -1,14 +1,7 @@
 function disableTakenSeats(seatIDs){
-   // for (x in seatIDs){
-        //$("#"+seatIDs[x]).attr("disabled", true);
-        //console.log(seatIDs[x]);
-        //print(seatIDs[x]);
-  // }
- // alert(seatIDs);
- console.log("Potato2");
- $("#AA01").prop("disabled", true);
+    $(document).on('ready', function(){
+        for (x in seatIDs){
+            $("#"+seatIDs[x]).attr("disabled", true);
+        }
+    });
 } 
-
-console.log("Potato");
-//$("AA01").attr("disabled", true);
-$(".container").on('click',disableTakenSeats);
