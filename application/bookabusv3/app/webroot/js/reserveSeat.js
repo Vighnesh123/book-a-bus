8995119180db2adbea0d7d0be532e46aa254c08e
aresteban <aresteban1@gmail.com>
2015-04-03 03:19:30 +0800

    Added a script that will calculate how much the user will pay. ver1

--- application/bookabusv3/app/webroot/js/reserveSeat.js
+++ application/bookabusv3/app/webroot/js/reserveSeat.js
@@ -1,7 +1,24 @@
 function disableTakenSeats(seatIDs){
     $(document).on('ready', function(){
         for (x in seatIDs){
-            $("#"+seatIDs[x]).attr("disabled", true);
+            $('#'+seatIDs[x]).attr('disabled', true);
         }
     });
-} 
+}
+
+function calculatePrice(price){
+    var priceTotal = 0.00;
+    price.toFixed(2);
+    $(document).ready(function (){
+        $('input:checkbox').on('change', function(){
+            if($(this).prop('checked')){
+                //alert(price);
+                priceTotal += price;
+            }else {
+                priceTotal -=price;
+            }
+            $('#price').text(priceTotal.toFixed(2));
+        });
+    });
+    
+}
