f3e18996f920b57d4e80e8b42ded1f62c4f6365c
aresteban <aresteban1@gmail.com>
2015-04-04 03:46:47 +0800

    Added code for a new row in database called transaction_code

--- application/bookabusv3/app/Model/Booking.php
+++ application/bookabusv3/app/Model/Booking.php
@@ -72,6 +72,11 @@ class Booking extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+                'transaction_code' => array(
+                        'notEmpty' => array(
+                                'rule'=> array('notEmpty'),
+                        )
+                )
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
